0b83419cd2f1895dded5960ee6754db92deba0d2
Mihir Jain <jainmihir84@gmail.com>
2018-10-04 12:15:05 +0530

    added new mongo ip

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@
  */
 var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://13.232.229.255:27017/registeryourself');
+mongoose.connect('mongodb://13.233.78.91:27017/registeryourself');
 
 
 //check if we are connected successfully or not
